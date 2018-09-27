a90a77b14caf019b26bf4450c73c1021a497a6f9
dev-assassin <alexandra.uefa@gmail.com>
2018-09-27 21:46:32 +0800

    add firebase authentication using angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBYxlz34glJMAkjUqIj1SkCnt-VwlYuSns',
+    authDomain: 'angular-firebase-ef7ca.firebaseapp.com',
+    databaseURL: 'https://angular-firebase-ef7ca.firebaseio.com',
+    projectId: 'angular-firebase-ef7ca',
+    storageBucket: 'angular-firebase-ef7ca.appspot.com',
+    messagingSenderId: '883599590897'
+  }
+};
